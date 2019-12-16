9442a037e27e029cf2ccf44447b80ed729484915
Adi Fine <adif@Adis-MacBook-Pro.local>
2019-12-16 23:44:59 +0200

    removed old files

--- app/boards/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls import url
-from django.contrib import admin
-from . import views
-
-urlpatterns = [
-    path('create', views.RestaurantListCreate.as_view() ),
-    url(r'^admin/', admin.site.urls),
-]
